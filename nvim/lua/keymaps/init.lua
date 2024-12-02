17b081c301f4b80bc77849e6de3e6630c404b092
Cuadra <jose.cuadra@live.com>
2024-12-02 16:27:04 -0500

    rest of the nvim config

---
+++ nvim/lua/keymaps/init.lua
@@ -0,0 +1,62 @@
+local opts = { noremap = true, silent = true}
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- keymap("n", "<leader>e", ":Lex 30<cr>", opts)
+
+
+-- Navigating buffers
+
+keymap("n", "e", ":bprevious<cr>", opts)
+keymap("n", "r", ":bnext<cr>", opts)
+
+
+-- New Buffer
+keymap("n", "<leader>n", ":enew<cr>", opts)
+
+-- Writing and Exiting
+keymap("n", "<leader>s", ":w<cr>", opts)
+keymap("n", "<leader>w", ":bd<cr>", opts)
+
+keymap("n", "<leader>h", "0", opts)
+keymap("n", "<leader>l", "$", opts)
+
+-- Stay in indent mode
+keymap("v", "h", "<gv^", opts)
+keymap("v", "l", ">gv^", opts)
+
+-- Move text up and down
+
+keymap("n", "<S-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("n", "<S-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("v", "<S-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<S-k>", ":m '<-2<CR>gv=gv", opts)
+keymap("x", "<S-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("x", "<S-k>", ":m '<-2<CR>gv=gv", opts)
+-- keymap("x", "J", ":m '>+1<CR>gv=gv", opts)
+-- keymap("x", "K", ":m '<-2<CR>gv=gv", opts)
+
+-- Find and get ready to replace
+keymap("n", "<leader>a", ":%s/", opts)
+
+-- Telescope
+keymap("n", "<leader>f", ":Telescope find_files<cr>", opts)
+keymap("n", "<leader>g", ":Telescope live_grep<cr>", opts)
+
+
+-- Oil
+keymap('n', '-', ':edit .<cr>', opts)
+
+-- copy and pasting
+-- yw to copy word
+-- yy to copy line
+-- p to paste
+-- d to delete
+-- dw to delete word
+-- dd to delete line
+
+
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,114 @@
+
+vim.wo.relativenumber = true
+vim.wo.number = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone","--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+local plugins = {
+  { 'github/copilot.vim'}, 
+  { "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+
+    conform.setup({
+      formatters_by_ft = {
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+        css = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        yaml = { "prettier" },
+        markdown = { "prettier" },
+        graphql = { "prettier" },
+        lua = { "stylua" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>#", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end,
+},
+  {
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add any options here
+  },
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+    }
+},
+  {
+  'stevearc/oil.nvim',
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  -- Optional dependencies
+  dependencies = {{'echasnovski/mini.icons', opts = {} }}
+},
+{
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+  'neovim/nvim-lspconfig'
+},
+{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+{
+  'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' }
+},
+{
+'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }
+}
+}
+local opts = {}
+--require('oil').setup()
+require('lazy').setup(plugins, opts)
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed={'ts_ls'}
+})
+require("lualine").setup()
+
+local lspconfig = require('lspconfig')
+lspconfig.ts_ls.setup({})
+
+local builtin = require("telescope.builtin")
+--vim.keymap.set('n', '<C-f', builtin.find_files, {})
+--vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
+require("catppuccin").setup({transparent_background = false})
+
+vim.cmd.colorscheme "catppuccin"
+--vim.cmd [[
+  --highlight Normal guibg=none
+  --highlight NonText guibg=none
+  --highlight Normal ctermbg=none
+  --highlight NonText ctermbg=none
+--]]
